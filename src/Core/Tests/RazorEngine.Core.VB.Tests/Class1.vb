f86efeb9bc69c3fd22a99f9351e29edb37403e9f
HowardRichards <dev@conficient.com>
2014-10-28 16:40:44 +0000

    Created a VB.NET test project for IsAnonymousType

---
+++ src/Core/Tests/RazorEngine.Core.VB.Tests/Class1.vb
@@ -0,0 +1,51 @@
+﻿Imports NUnit.Framework
+
+
+<TestFixture>
+Public Class CompilerServicesUtility_Tests
+
+    ''' <summary>
+    ''' Validate that VB compiles anonymous types with prefix "VB$Anonymous"
+    ''' </summary>
+    ''' <remarks></remarks>
+    <Test>
+    Public Sub IsAnonymousType_ValidateVBAnonTypePrefix()
+        'Create an anonymous VB type
+        Dim anon = New With {.Name = "Test", .ID = 1}
+
+        Dim type = anon.GetType()
+        Const prefix = "VB$Anonymous"
+        Const expected = True
+
+        Dim actual = type.Name.StartsWith(prefix)
+
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+
+    <Test>
+    Public Sub IsAnonymousType_TestWhenTrue()
+        'Create an anonymous VB type
+        Dim anon = New With {.Name = "Test", .ID = 1}
+
+        Dim type = anon.GetType()
+        Const expected = True
+
+        Dim actual = RazorEngine.Compilation.CompilerServicesUtility.IsAnonymousType(type)
+
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+    <Test>
+    Public Sub IsAnonymousType_TestWhenFalse()
+        'Get a named type
+        Dim type = GetType(String)
+        Const expected = False
+
+        Dim actual = RazorEngine.Compilation.CompilerServicesUtility.IsAnonymousType(type)
+
+        Assert.AreEqual(expected, actual)
+    End Sub
+
+
+End Class
